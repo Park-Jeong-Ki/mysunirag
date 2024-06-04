7b2df55e0010fe5012448f687a3b345f12a3687a
jeongkpa <jeongkpa@gmail.com>
2024-06-05 04:29:35 +0900

    gpt-4o 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def main():
     with st.sidebar:
         model_selection = st.selectbox(
             "Choose the language model",
-            ("gpt-3.5-turbo", "gpt-4-turbo-preview"),
+            ("gpt-3.5-turbo", "gpt-4-turbo-preview", "gpt-4o"),
             key="model_selection"
         )
         uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx', 'pptx'], accept_multiple_files=True)
